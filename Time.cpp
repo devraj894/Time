a652a7a2dd99caea8c4af323158409f4a949ef67
devraj894 <118315477+devraj894@users.noreply.github.com>
2022-12-14 22:37:04 +0530

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -3,29 +3,38 @@ using namespace std;
 class Time{
     int hour,min,second;
     public:
-    void getData(){
-        cout<<"Enter the hour : ";
-        cin>>hour;
-        cout<<"Enter the minute : ";
-        cin>>min;
-        cout<<"Enter the second : ";
-        cin>>second;
+    void setData(int a, int b, int c){
+       hour = a;
+       min = b;
+       second = c;
     }
-    void showData(){
+    void display(){
         cout<<"Hour : "<<hour<<endl;
         cout<<"Minute : "<<min<<endl;;
         cout<<"Second : "<<second<<endl;
     }
-    int Result(){
-        cout<<"***RESULT***"<<endl;
-        int time = hour+min+second;
-        return time;
-    }
+   Time operator+(Time t2){
+       Time temp;
+       temp.hour = hour + t2.hour;
+       temp.min = min + t2.min;
+       temp.second = second + t2.second;
+       
+       temp.normalize();
+       return(temp);
+   }
+    void normalize(){
+        min = min+second / 60;
+        second = second % 60;
+        hour = hour + min / 60;
+        min = min % 60;
 };
 int main(){
-    Time t;
-    t.getData();
-    t.showData();
-    cout<<"Result : "<<t.Result();
+    Time t1,t2,t3;
+    t1.setData(3,35,26);
+    t2.setData(4,45,50);
+    t1.display();
+    t2.display();
+    t3 = t1.operator+(t2);
+    t3.display();
     return 0;
 }
